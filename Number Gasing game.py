4cbcceb6389a6b68b430611b686da00629f6ae4c
Sahil Laskar <Sahillaskar137@gmail.com>
2020-04-12 11:52:21 +0530

    This is a number guessing Game
    add a game

---
+++ Number Gasing game.py
@@ -0,0 +1,23 @@
+print("This is a number guessing Game")
+n=0
+while(n<=9):
+    n1=int(input("Guess between 1 to 100: \n"))
+    if n1>18:
+        print("You enter too high")
+        n=n+1
+        print(9-n,"Gases left")
+
+    elif n1<18:
+        print("You enter too Low")
+        n=n+1
+        print(9-n,"Gases left")
+
+    else:
+        print("You win")
+        print(n,"Number of guess to finish")
+        print(9-n,"Gases left")
+        break
+
+    if (n>9):
+     print("Game Over")
+     # 20 Epx
